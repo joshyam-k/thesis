d1260a61a67b47f8163ddbcdd2e3d29587f016e5
joshyam-k <yamamojo@reed.edu>
2022-10-04 11:25:22 -0700

    tests on grouped data

--- bayesian_simple_lm.R
+++ bayesian_simple_lm.R
@@ -13,8 +13,7 @@ library(broom.mixed)
 sim_dat <- tibble(
   x = rnorm(1000, 50, 9),
   error = rnorm(1000, 0, 36),
-  y = 150 + 4*x + error,
-  group = rep(c(1,2,3,4,5), 200)
+  y = 150 + 4*x + error
 )
 
 sim_dat %>% 
@@ -46,7 +45,7 @@ means <- c()
 for(i in 1:length(x_new)){
   curr <- model_df %>% 
     mutate(newd = x_new[i]) %>% 
-    mutate(mu = `(Intercept)` + x*70) %>% 
+    mutate(mu = `(Intercept)` + x*newd) %>% 
     rowwise() %>% 
     mutate(y_new = rnorm(1, mean = mu, sd = sigma)) %>% 
     ungroup()
@@ -76,4 +75,61 @@ data.frame(
 res_list[[1]] %>% 
   ggplot(aes(x = y_new)) +
   geom_density()
+
+
+
+## grouped data
+
+data(cherry_blossom_sample)
+running <- cherry_blossom_sample %>% 
+  select(runner, age, net)
+
+ggplot(running, aes(y = net, x = age)) + 
+  geom_point()
+
+
+wrong_model <- stan_glm(
+  net ~ age, 
+  data = running, family = gaussian, 
+  prior_intercept = normal(0, 2.5, autoscale = TRUE),
+  prior = normal(0, 2.5, autoscale = TRUE), 
+  prior_aux = exponential(1, autoscale = TRUE),
+  chains = 4, iter = 5000*2, seed = 84735)
+
+# Posterior summary statistics
+tidy(wrong_model, conf.int = TRUE, conf.level = 0.80)
+
+
+# Plot of the posterior median model
+ggplot(running, aes(x = age, y = net, group = runner)) + 
+  geom_smooth(method = "lm", se = FALSE, color = "gray", size = 0.5) + 
+  geom_abline(aes(intercept = 75.2, slope = 0.268), color = "blue")
+
+
+wrong_model_df <- as.data.frame(wrong_model)
+
+
+# predictions for new runners
+set.seed(100)
+new_dat <- runif(50, 50, 60)
+
+
+new_res_list <- list()
+new_means <- c()
+
+for(i in 1:length(new_dat)){
+  curr <- wrong_model_df %>% 
+    mutate(newd = new_dat[i]) %>% 
+    mutate(mu = `(Intercept)` + age*newd) %>% 
+    rowwise() %>% 
+    mutate(y_new = rnorm(1, mean = mu, sd = sigma)) %>% 
+    ungroup()
+  
+  new_res_list[[i]] <- curr
+  m_i <- mean(curr$y_new)
+  new_means <- c(new_means, m_i)
+  
+}
+
+
 
