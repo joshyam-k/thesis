a62fca533246734879f4f07c2e385be4d19198e5
joshyam-k <yamamojo@reed.edu>
2022-10-11 09:04:56 -0700

    simulations are done

--- R/coverage_testing.R
+++ R/coverage_testing.R
@@ -213,6 +213,68 @@ for (t in 1:500){
 }
 
 
+
+## Setting 4
+# still using DGP from setting 3
+
+covers_s4 <- data.frame()
+
+for(t in 1:50){
+  
+  s4_full <- DGP_s3(12500)
+  s4_build <- s4_full %>% head(10000)
+  s4_test <- s4_full %>% tail(2500) %>% 
+    filter(group == 1)
+  
+  model <- stan_glmer(
+    Y ~ X + (1 | group), 
+    data = s4_build, family = gaussian,
+    prior_intercept = normal(200, 10),
+    prior = normal(4, 2), 
+    prior_aux = exponential(1, autoscale = TRUE),
+    prior_covariance = decov(reg = 1, conc = 1, shape = 1, scale = 1),
+    chains = 4, iter = 5000*2, seed = 84735, 
+    prior_PD = F)
+  
+  model_df <- as.data.frame(model)
+  full_preds <- data.frame()
+  
+  for(i in 1:nrow(s4_test)) {
+    curr <- model_df %>% 
+      rename(b = `b[(Intercept) group:1]`) %>% 
+      select(`(Intercept)`, X, b, sigma) %>% 
+      mutate(mu_grp1 = `(Intercept)` + X*s4_test$X[i] + b,
+             y_new = rnorm(20000, mu_grp1, sd = sigma)) %>% 
+      mutate(iter = row_number())
+    
+    full_preds <- rbind(full_preds, curr)
+    
+  }
+  
+  full_preds_means <- full_preds %>% 
+    group_by(iter) %>% 
+    summarise(pred = mean(y_new)) 
+  
+  q <- quantile(full_preds_means$pred, probs = c(0.025, 0.975))
+  
+  to_add <- tibble(
+    true = mean(s4_test$Y),
+    lower = q[1],
+    upper = q[2]
+  )
+  
+  covers_s4 <- rbind(covers_s4, to_add) 
+  
+  print(paste0("done with trial ", t))
+  
+ 
+
+}
+
+
+
+
+
 # helper function to extract coverage stat
 coverage <- function(df){
   df %>% 
