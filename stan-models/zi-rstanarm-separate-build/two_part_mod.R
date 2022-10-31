840abec47d208adb2b2cf77a85e17c97c1bf3c01
joshyam-k <yamamojo@reed.edu>
2022-10-31 10:51:53 -0700

    deleted old files and updated two part mod file

--- stan-models/zi-rstanarm-separate-build/two_part_mod.R
+++ stan-models/zi-rstanarm-separate-build/two_part_mod.R
@@ -11,11 +11,16 @@ dat <- dat_raw %>%
   ungroup() %>% 
   mutate(DRYBIO_AG_INDICATOR = ifelse(DRYBIO_AG_TPA_live_ADJ > 0, 1, 0))
 
+
+dat %>% 
+  ggplot(aes(x = tcc, y = DRYBIO_AG_TPA_live_ADJ)) +
+  geom_point()
+
 dat_y_mod <- dat %>% 
   filter(DRYBIO_AG_TPA_live_ADJ > 0)
 
 mod_y <- stan_glmer(
-  DRYBIO_AG_TPA_live_ADJ ~ tcc + (1 | group_id), 
+  DRYBIO_AG_TPA_live_ADJ ~ tcc + tnt + (1 | group_id), 
   data = dat_y_mod, family = gaussian,
   prior_intercept = normal(100, 100),
   prior = normal(2.5, 100), 
@@ -26,7 +31,7 @@ mod_y <- stan_glmer(
 )
 
 mod_p <- stan_glmer(
-  DRYBIO_AG_INDICATOR ~ tcc + (1 | group_id), 
+  DRYBIO_AG_INDICATOR ~ tcc + tnt + (1 | group_id), 
   data = dat, family = binomial,
   prior_intercept = normal(0, 2.5, autoscale = TRUE),
   prior = normal(0, 2.5, autoscale = TRUE), 
@@ -36,8 +41,43 @@ mod_p <- stan_glmer(
 )
 
 
-res <- as.data.frame(mod_p)
+res_p <- as.data.frame(mod_p)
+res_y <- as.data.frame(mod_y)
 
-res %>% 
-  ggplot(aes(x = `b[(Intercept) group_id:2]`)) +
+
+new_x <- 30
+
+prediction <- function(new_x, group, df_p, df_y) {
+  
+    name_match <- paste0("b[(Intercept) group_id:", group, "]")
+    
+    # select correct group col
+    df_y <- select(df_y, c(name_match, `(Intercept)`, tcc, sigma)) %>% 
+      rename(b = name_match) 
+    
+    df_p <- select(df_p, c(name_match, `(Intercept)`, tcc)) %>% 
+      rename(b = name_match)
+    
+    names(df_y) <- paste0(names(df_y), "_y")
+    names(df_p) <- paste0(names(df_p), "_p")
+    
+    full_params <- cbind(df_y, df_p)
+
+    full_params %>% 
+      mutate(
+        mu = b_y + `(Intercept)_y` + tcc_y*new_x,
+        pr = (exp(b_p + `(Intercept)_p` + tcc_p*new_x)/(1 + exp(b_p + `(Intercept)_p` + tcc_p*new_x))),
+        y_hat = rnorm(20000, mean = mu, sd = sigma_y),
+        p_hat = rbinom(20000, 1, pr),
+        y_final = y_hat*p_hat
+      ) 
+}
+
+prediction(10, 20, res_p, res_y) %>% 
+  ggplot(aes(x = y_final)) +
   geom_density()
+
+
+    
+
+
